5dfd9a7f7236c506102b3b3de1efbc3ffc56dfb2
n-ob2 <n_ob2@hotmail.com>
2021-09-25 17:48:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/animalbook/SubActivity.kt
@@ -0,0 +1,12 @@
+package com.example.animalbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.animalbook.R
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
